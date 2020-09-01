0d58f3e5cdb06336b7f7ad368aa50d264249ec51
danuka-07 <68368975+danuka-07@users.noreply.github.com>
2020-09-02 00:06:33 +0530

    Update cloneHelper.py

--- tobrot/helper_funcs/cloneHelper.py
+++ tobrot/helper_funcs/cloneHelper.py
@@ -146,7 +146,7 @@ class CloneHelper:
             button_markup = pyrogram.InlineKeyboardMarkup(button)
             msg = await self.lsg.edit_text(
                 f"🤖: {_up} cloned successfully in your Cloud <a href='tg://user?id={self.u_id}'>🤒</a>\
-                \n♨ Info: Calculating...",
+                \♨ Info: Calculating...",
                 reply_markup=button_markup,
                 parse_mode="html"
             )
