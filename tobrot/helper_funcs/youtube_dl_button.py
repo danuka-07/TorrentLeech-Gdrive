a81b2418826fc94ea603c52f6408a98f42bf1050
danuka-07 <68368975+danuka-07@users.noreply.github.com>
2020-09-01 23:59:50 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -45,7 +45,7 @@ async def youtube_dl_call_back(bot, update):
     if current_user_id != current_touched_user_id:
         await bot.answer_callback_query(
             callback_query_id=update.id,
-            text="who are you? 🤪👹😈💀",
+            text="who are you? 🤪👹👿💀",
             show_alert=True,
             cache_time=0
         )
